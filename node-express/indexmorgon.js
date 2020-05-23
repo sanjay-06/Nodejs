f654039290225f38c26b2066b69ef3a2ba617073
sanjay <san692001@gmail.com>
2020-05-23 15:50:36 +0530

    Router

--- node-express/indexmorgon.js
+++ node-express/indexmorgon.js
@@ -19,5 +19,5 @@ app.use((req,res,next)=>{
 const server =http.createServer(app);
 
 server.listen(port,hostname,()=>{
-    console.log(`Server running at http://${hostname}:${port}`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
