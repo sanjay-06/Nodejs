037758ff346527b9aee54992c9251e7d637e9065
sanjay <san692001@gmail.com>
2020-05-23 10:05:48 +0530

    simple node module

--- indexexp.js
+++ indexexp.js
@@ -1,4 +1,8 @@
-var rect =require("./rectangle");
+var rect = {
+    perimeter:(x,y) => (2*(x+y)),
+    area:(x,y) => (x*y)
+};
+
 function solveRect(l,b){
  console.log("Solving for rectangle with l ="+ l +"and b ="+b);
  if(l<=0||b<=0)
@@ -14,4 +18,4 @@ function solveRect(l,b){
 solveRect(2,4);
 solveRect(3,5);
 solveRect(0,5);
-solveRect(-3,5); 
+solveRect(-3,5);
