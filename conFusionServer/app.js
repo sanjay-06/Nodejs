364b349be255c9ff33fd8ae9b00b7b4c348da7f0
sanjay <san692001@gmail.com>
2020-05-26 11:11:51 +0530

    Express Generator REST API

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -9,6 +9,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var indexRouter=require('./routes/index');
+var usersRouter=require('./routes/users');
+var dishRouter=require('./routes/dishRouter');
+var promoRouter=require('./routes/promoRouter');
+var leaderRouter=require('./routes/leaderRouter');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
